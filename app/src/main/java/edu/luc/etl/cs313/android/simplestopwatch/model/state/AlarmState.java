860a2d4b3abdd258fe11e2a6e9cc01e25a419e93
Ryan <rsabo@luc.edu>
2022-08-06 15:32:13 -0500

    I increased the alarm sound off time to 3 seconds.  This way the multiple method calls to the media player isn't stacking, there's enough time now for the sound to play before the next one does.  So the stacked calls wont carry over to the stopped state.

--- app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/state/AlarmState.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/state/AlarmState.java
@@ -19,47 +19,19 @@ class AlarmState implements StopwatchState {
     @Override
     public void onStartStop() {
         sm.actionReset();
-//        sm.actionStart();
-//        sm.toIncrementState();
-        //Method to stop alarm
-
         sm.actionStop();
         counter=1;
         sm.toStoppedState();
 
     }
 
-    /*
-    @Override
-    public void onLapReset() {
-        sm.toStoppedState();
-        sm.actionUpdateView();
-    }
-
-     */
 
     @Override
     public void onTick() {
         counter++;
-        if(counter%2==0){sm.playAlarm();}
-
-//        while(alarmOn){
-//             sm.playAlarm();
-//
-//             alarmOn=false;
-//
-//            //TimeUnit.SECONDS.sleep(1);
-//        }
-        //Sound the alarm with alternate sound at every tick.
+        if(counter%3==0){sm.playAlarm();}
     }
 
-    //Old Version being commented out.  We likely have to update the regular UIruntime.  I'll do that below it.
-    /*
-    @Override
-    public void updateView() {
-        sm.updateUILaptime();
-    }
-     */
 
     @Override
     public void updateView() {
