9fbcc4d03751158fdf03d772e7f5663f9420007c
GioMonte5 <jerome32891@gmail.com>
2022-08-08 21:16:21 -0500

    removed un-needed comments

--- app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/state/DefaultTimerStateMachine.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/state/DefaultTimerStateMachine.java
@@ -42,13 +42,10 @@ public class DefaultTimerStateMachine implements TimerStateMachine {
     // these must be synchronized because events can come from the
     // UI thread or the timer thread
     @Override public synchronized void onMultiButton() { state.onMultiButton(); }
-    //@Override public synchronized void onLapReset()  { state.onLapReset(); }
     @Override public synchronized void onTick()      { state.onTick(); }
 
     @Override public void updateUIRuntime() { listener.onTimeUpdate(timeModel.getRuntime()); }
 
-
-    //@Override public void updateUILaptime() { listener.onTimeUpdate(timeModel.getLaptime()); }
 
     // known states
     private final TimerState STOPPED     = new StoppedState(this);
